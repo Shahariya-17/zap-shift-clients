bdf847ec09096d3d1b2df4b01d748925a183d925
Shahariya-17 <shahariyarrakib780@gmail.com>
2025-07-01 13:03:04 +0600

    added card payment amount

--- src/pages/Dashboard/Payment/PaymentForm.jsx
+++ src/pages/Dashboard/Payment/PaymentForm.jsx
@@ -1,11 +1,31 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useQuery } from "@tanstack/react-query";
 import React, { useState } from "react";
+import { useParams } from "react-router";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const PaymentForm = () => {
   const stripe = useStripe();
   const elements = useElements();
-
   const [error, setError] = useState('');
+  const {parcelId} = useParams();
+  const axiosSecure = useAxiosSecure();
+  
+
+  const {isPending, data: parcelInfo = {}} = useQuery({
+    queryKey: ['parcels', parcelId],
+    queryFn: async()=>{
+        const res = await axiosSecure.get(`/parcels/${parcelId}`);
+        return res.data;
+    }
+
+  })
+
+  if(isPending){
+    return <span className="loading loading-spinner text-success"></span>
+  }
+  console.log(parcelInfo);
+  const amount = parcelInfo.cost;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -43,7 +63,7 @@ const PaymentForm = () => {
           type="submit"
           disabled={!stripe}
         >
-          Pay for parcel pickup
+          Pay ${amount}
         </button>
         {
             error && <p className="text-center text-red-500">{error}</p>
